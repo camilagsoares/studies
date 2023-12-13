1372a57d3bee71edd841d34060172acee09e459e
camilagsoares <camila.soares19@hotmail.com>
2023-12-13 10:32:31 -0300

    .

---
+++ ts/src/index.js
@@ -0,0 +1,17 @@
+var age = 5;
+var firstName = "Camila";
+var isValid = true;
+var idk = 5;
+//A lista(array) de nomes deve ser somente do tipo String
+var listaNomes = ["Camila", "Lu"];
+// Tupla - Ordem exata dos valores que a gente quer
+var person = [3, 'Camila'];
+// Lista de tuplas que vai ser uma lista
+var people = [
+    [1, 'Jane'],
+    [2, 'Doe']
+];
+// Intersections - ser uma coisa ou outra
+var productId = 1;
+// Enum
+console.log(age);
--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,9 +1,25 @@
 let age: number = 5;
-const firstName: string = 'Camila'
+const firstName: string = "Camila";
 const isValid: boolean = true;
-let idk: any = 5
+let idk: any = 5;
+
+//A lista(array) de nomes deve ser somente do tipo String
+const listaNomes: string[] = ["Camila", "Lu"];
 
 
-//A lista(array) de nomes deve ser somente do tipo String 
-const listaNomes: string[] = ['Camila','Lu']
-console.log(age)
+// Tupla - Ordem exata dos valores que a gente quer
+const person: [number,string] = [3,'Camila']
+
+// Lista de tuplas que vai ser uma lista
+const people: [number,string][] = [
+    [1,'Jane'],
+    [2,'Doe']
+]
+
+// Intersections - ser uma coisa ou outra
+const productId: string | number | boolean = 1
+
+// Enum
+
+
+console.log(age);
