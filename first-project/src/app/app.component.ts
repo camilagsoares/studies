4b4c0e7875122d5458ddfa51e5256fcaf957fe4f
camilagsoares <camila.soares19@hotmail.com>
2024-05-09 14:02:43 -0300

    .

--- first-project/src/app/app.component.ts
+++ first-project/src/app/app.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NewComponent } from './components/new-component/new-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NewComponent],
+  styles: [`
+  h1 {
+    color:red;
+  }
+  `],
   template: `
+  <app-new-component />
   <h1>Curso de Angular</h1>
-  <h2>Components</h2>
-  
 <!-- <router-outlet /> -->
   `,
 })
-export class AppComponent {}
+export class AppComponent { }
