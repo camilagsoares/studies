c8ed264f067d7e1eafac6a19f7361edd027ab80b
camilagsoares <camila.soares19@hotmail.com>
2024-05-18 09:39:21 -0300

    .

--- first-project/src/app/app.component.ts
+++ first-project/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NewComponent } from './components/new-component/new-component.component';
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
+import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NewComponent, TemplateBindingComponent],
+  imports: [RouterOutlet, NewComponent, TemplateBindingComponent,TemplateVariablesComponent],
   styles: [`
   h1 {
     color:red;
@@ -15,6 +16,7 @@ import { TemplateBindingComponent } from './components/template/template-binding
   template: `
   <app-new-component />
   <app-template-binding />
+  <app-template-variables />
   <h1>Curso de Angular</h1>
 <!-- <router-outlet /> -->
   `,
---
+++ first-project/src/app/components/template/template-variables/template-variables.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-variables',
+  standalone: true,
+  imports: [],
+  templateUrl: './template-variables.component.html',
+  styleUrl: './template-variables.component.scss'
+})
+export class TemplateVariablesComponent {
+
+  
+}
