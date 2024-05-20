e21fad830e772503ff4e98d784fb0ccf85efd35e
camilagsoares <camila.soares19@hotmail.com>
2024-05-20 20:47:52 -0300

    .

--- angular/first-project/src/app/app.component.ts
+++ angular/first-project/src/app/app.component.ts
@@ -3,22 +3,25 @@ import { RouterOutlet } from '@angular/router';
 import { NewComponent } from './components/new-component/new-component.component';
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
 import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
+import { TemplateControlFlowComponent } from './components/template/template-control-flow/template-control-flow.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NewComponent, TemplateBindingComponent,TemplateVariablesComponent],
+  imports: [RouterOutlet, NewComponent, TemplateBindingComponent, TemplateVariablesComponent, TemplateControlFlowComponent],
   styles: [`
   h1 {
     color:red;
   }
   `],
   template: `
-  <app-new-component />
+  <!-- <app-new-component />
   <app-template-binding />
-  <app-template-variables />
+  <app-template-variables /> -->
   <h1>Curso de Angular</h1>
 <!-- <router-outlet /> -->
+<app-template-control-flow />
+
   `,
 })
 export class AppComponent { }
