4afa38c81bc754a1d885ea6554f531d2fd70703f
camilagsoares <camila.soares19@hotmail.com>
2024-05-22 06:16:18 -0300

    .

--- angular/first-project/src/app/components/template/template-control-flow/template-control-flow.component.ts
+++ angular/first-project/src/app/components/template/template-control-flow/template-control-flow.component.ts
@@ -1,9 +1,10 @@
+import { NgSwitch } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-template-control-flow',
   standalone: true,
-  imports: [],
+  imports: [NgSwitch],
   templateUrl: './template-control-flow.component.html',
   styleUrl: './template-control-flow.component.scss'
 })
