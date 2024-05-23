82710f59bc0c41c2f1a6320f1f9883ca0f5cb54e
camilagsoares <camila.soares19@hotmail.com>
2024-05-23 11:06:02 -0300

    .

--- angular/portfolio/src/modules/portfolio/components/experiences/experiences.component.ts
+++ angular/portfolio/src/modules/portfolio/components/experiences/experiences.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { IExperiences } from '../../interface/IExperiences.interface';
 
 @Component({
   selector: 'app-experiences',
   standalone: true,
   imports: [],
   templateUrl: './experiences.component.html',
-  styleUrl: './experiences.component.scss'
+  styleUrl: './experiences.component.scss',
 })
 export class ExperiencesComponent {
+  public arrayExperiences = signal<IExperiences[]>([
+    {
+      summary: {
+        strong: 'Lorem Ipsum is simply dummy',
+        p: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+      },
+      text: 'when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also.',
+    },
 
+    {
+      summary: {
+        strong: 'Lorem Ipsum is simply dummy',
+        p: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+      },
+      text: 'when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also.',
+    },
+    {
+      summary: {
+        strong: 'Lorem Ipsum is simply dummy',
+        p: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+      },
+      text: 'when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also.',
+    },
+  ]);
 }
---
+++ angular/portfolio/src/modules/portfolio/interface/IProjects.interface.ts
@@ -0,0 +1,9 @@
+export interface IProjects {
+    src: string;
+    alt: string;
+    title: string;
+    height: string;
+    width: string;
+    description: string;
+    links: [{name: string, href: string}]
+}
