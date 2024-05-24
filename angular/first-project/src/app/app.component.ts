cdb5f076fc4853d56f19f0a782d0b3c3f3a730fe
camilagsoares <camila.soares19@hotmail.com>
2024-05-24 09:19:37 -0300

    .

--- angular/first-project/src/app/app.component.ts
+++ angular/first-project/src/app/app.component.ts
@@ -4,24 +4,34 @@ import { NewComponent } from './components/new-component/new-component.component
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
 import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
 import { TemplateControlFlowComponent } from './components/template/template-control-flow/template-control-flow.component';
+import { PaiOuMaeComponent } from './components/comunicacao-entre-components/pai-ou-mae/pai-ou-mae.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NewComponent, TemplateBindingComponent, TemplateVariablesComponent, TemplateControlFlowComponent],
-  styles: [`
-  h1 {
-    color:red;
-  }
-  `],
+  imports: [
+    RouterOutlet,
+    NewComponent,
+    TemplateBindingComponent,
+    TemplateVariablesComponent,
+    TemplateControlFlowComponent,
+    PaiOuMaeComponent,
+  ],
+  styles: [
+    `
+/*       h1 {
+        color: red;
+      } */
+    `,
+  ],
   template: `
-  
-  <!-- <app-new-component />
+    <!-- <app-new-component />
   <app-template-binding />
   <app-template-variables /> -->
-  <h1>Curso de Angular</h1>
-<!-- <router-outlet /> -->
-<app-template-control-flow />
+    <h1>Curso de Angular</h1>
+    <!-- <router-outlet /> -->
+    <!--     <app-template-control-flow /> -->
+    <app-pai-ou-mae />
   `,
 })
-export class AppComponent { }
+export class AppComponent {}
